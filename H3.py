b03670c3d725255ec58c149f4774f0b88c73f6b8
ekkuan <erickkuan@gmail.com>
2017-03-11 21:51:16 -0800

    first commit

--- H3.py
+++ H3.py
@@ -9,14 +9,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from flask import Flask
 
-app = Flask(__name__)
-
-
-
-predict = pandas.read_csv("trafficData.csv")
-# Print the names of the columns in games.
-print(predict.columns) #columns = schema
-print(predict.road_id)
+# app = Flask(__name__)
 
 '''# Make a histogram of all the ratings in the average_rating column.
 plt.hist(predict["traffic_status"])
@@ -43,41 +36,46 @@ plt.scatter(x=plot_columns[:,0], y=plot_columns[:,1], c=labels)
 plt.show();'''
 
 
+# Read the CSV file with pandas. Use predict.[column name] to get column values.
+predict = pandas.read_csv("trafficData.csv")
+
 columns = predict.columns.tolist()
 target = "traffic_status"
-
+# road_id = predict.road_id
+# direction = predict.direction
+# dayOfWeek = predict.dayOfWeek
+# timeOfDay = predict.timeOfDay
+# traffic = predict.traffic_status
 # Generate the training set.  Set random_state to be able to replicate results.
 train = predict.sample(frac=.8, random_state=1)
-# Select anything not in the training set and put it in the testing set.
+# # Select anything not in the training set and put it in the testing set.
 test = predict.loc[~predict.index.isin(train.index)]
 
-# Print the shapes of both sets.
-print(train.shape)
-print(test.shape)
-print(test)
-print(train)
-
-# Initialize the model class.
 model = LinearRegression()
 # Fit the model to the training data.
 model.fit(train[columns], train[target])
 
+# Print the shapes of both sets.
+# print(train.shape)
+# print(test.shape)
+# print(test)
+# print(train)
+
 # Generate our predictions for the test set.
 predictions = model.predict(test[columns])
-
 print(predictions) # predicting traffic status 5,1,6,18,9
-#outputs 8.85 which is pretty close to 9
+# outputs 8.85 which is pretty close to 9
 
 # Compute error between our test predictions and the actual values.
-percentError = mean_squared_error(predictions, test[target]) * 100
+# percentError = mean_squared_error(predictions, test[target]) * 100
+# print(percentError)
 
-print(percentError)
-
-@app.route("/")
-def hello():
-    return "testing"
-
-if __name__ == "__main__":
-    app.run()
-
+#
+#
+# @app.route("/")
+# def hello():
+#     return "testing"
+#
+# if __name__ == "__main__":
+#     app.run()
 
